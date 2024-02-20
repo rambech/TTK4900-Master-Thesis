5981804217f2e4ccb7b4e75fdb01e018f043edae
rambech <thatalexram@gmail.com>
2024-02-20 15:02:30 +0100

    Fix log naming

--- src/plotting/data.py
+++ src/plotting/data.py
@@ -6,8 +6,8 @@ import json
 def save_data(data: dict, name: str) -> None:
     log_dir = "log_data/logs"
     assert (os.path.exists(log_dir)), f"{log_dir} does not exist"
-    current_time = datetime.now()
-    log_name = name + current_time.strftime("%m-%d-h-M")
+    current_time = datetime.now().strftime("%m-%d-%H-%M")
+    log_name = name + current_time
     file_name = f"{log_name}.json"
     log_path = os.path.join(log_dir, file_name)
 
