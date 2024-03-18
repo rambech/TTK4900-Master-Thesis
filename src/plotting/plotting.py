a0cce01bf24226dda9edf55963acd5ba121fc080
rambech <thatalexram@gmail.com>
2024-03-18 15:09:07 +0100

    Add velocity plots and round to nearest 5 decimals

--- src/plotting/plotting.py
+++ src/plotting/plotting.py
@@ -61,38 +61,56 @@ def plot_solution(solution: Opti.solve, x: Opti.variable, u: Opti.variable, slac
     """
     Plot optimizer solution
     """
+    dt = 0.05
+
+    x_data = np.round(solution.value(x), 5)[:, :-2]
+    u_data = np.round(solution.value(u), 5)
+    t_data = np.linspace(0, len(u_data), num=len(u_data[0]))
 
     # Position plot
     fig0, ax0 = plt.subplots(figsize=(7, 7))
-
-    ax0.plot(solution.value(x[0, :]), solution.value(x[1, :]))
+    ax0.plot(x_data[0, :], x_data[1, :])
     ax0.set(xlabel="x position", ylabel="y position")
 
     # Position/time plot x
     fig1, ax1 = plt.subplots(figsize=(7, 7))
 
-    ax1.plot(solution.value(x[0, :]))
+    ax1.plot(x_data[0, :])
     ax1.set(xlabel="t", ylabel="x position")
 
     # Position/time plot y
     fig2, ax2 = plt.subplots(figsize=(7, 7))
 
-    ax2.plot(solution.value(x[1, :]))
+    ax2.plot(x_data[1, :])
     ax2.set(xlabel="t", ylabel="y position")
 
     # Heading plot psi
     fig3, ax3 = plt.subplots(figsize=(7, 7))
-    ax3.plot(solution.value(x[2, :]))
+    ax3.plot(x_data[2, :])
     ax3.set(xlabel="t", ylabel="$\psi$ heading")
 
     # U
     fig4, ax4 = plt.subplots(figsize=(7, 7))
-
-    ax4.plot(solution.value(u[0, :]))
-    ax4.plot(solution.value(u[1, :]))
+    ax4.step(t_data, u_data[0, :],  where="post")
+    ax4.step(t_data, u_data[1, :], where="post")
     ax4.set(xlabel="t", ylabel="u control")
 
+    # Print rates
+    if len(solution.value(x[:, 0])) > 3:
+        fig5, ax5 = plt.subplots(figsize=(7, 7))
+        ax5.plot(solution.value(x[3, :]))
+        ax5.set(xlabel="t", ylabel="$u$ surge")
+
+        fig5, ax5 = plt.subplots(figsize=(7, 7))
+        ax5.plot(x_data[4, :])
+        ax5.set(xlabel="t", ylabel="$v$ sway")
+
+        fig5, ax5 = plt.subplots(figsize=(7, 7))
+        ax5.plot(x_data[5, :])
+        ax5.set(xlabel="t", ylabel="$r$ heading rate")
+
     if slack is not None:
+        s_data = np.round(solution.value(slack), 5)
         pass
 
     plt.show()
