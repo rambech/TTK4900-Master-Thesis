bda0fca205967c0d1024888326712f67e051063d
rambech <thatalexram@gmail.com>
2024-03-18 13:36:14 +0100

    Greatly simplify OtterModel step method

--- src/vehicle/models/otter_model.py
+++ src/vehicle/models/otter_model.py
@@ -21,29 +21,35 @@ class OtterModel(Model):
         s = opti.variable(6, self.N+1)
 
         # Control signal and time constraint
-        opti.subject_to(opti.bounded(self.n_min, u[0, :], self.n_max))
-        opti.subject_to(opti.bounded(self.n_min, u[1, :], self.n_max))
+        # opti.subject_to(opti.bounded(self.n_min, u[0, :], self.n_max))
+        # opti.subject_to(opti.bounded(self.n_min, u[1, :], self.n_max))
+        # opti.subject_to(opti.bounded(-100, u[0, :], 100))
+        # opti.subject_to(opti.bounded(-100, u[1, :], 100))
 
         # Boundary values
-        # Initial conditions
+        # Initial state conditions
         opti.subject_to(x[0, 0] == x_init[0])
         opti.subject_to(x[1, 0] == x_init[1])
         opti.subject_to(x[2, 0] == x_init[2])
         opti.subject_to(x[3, 0] == x_init[3])
         opti.subject_to(x[4, 0] == x_init[4])
         opti.subject_to(x[5, 0] == x_init[5])
-        opti.subject_to(u[0, 0] == u_init[0])
-        opti.subject_to(u[1, 0] == u_init[1])
 
-        # Initial guesses
+        # Initial control input conditions
+        # opti.subject_to(u[0, 0] == u_init[0])
+        # opti.subject_to(u[1, 0] == u_init[1])
+
+        # Initial guesses for state variables
         opti.set_initial(x[0, :], x_init[0])
         opti.set_initial(x[1, :], x_init[1])
         opti.set_initial(x[2, :], x_init[2])
         opti.set_initial(x[3, :], x_init[3])
         opti.set_initial(x[4, :], x_init[4])
         opti.set_initial(x[5, :], x_init[5])
-        opti.set_initial(u[0, :], u_init[0])
-        opti.set_initial(u[1, :], u_init[1])
+
+        # # Initila guesses for control inputs
+        # opti.set_initial(u[0, :], u_init[0])
+        # opti.set_initial(u[1, :], u_init[1])
 
         return x, u, s
 
@@ -105,12 +111,14 @@ class OtterModel(Model):
         self.l1 = -y_pont  # lever arm, left propeller (m)
         self.l2 = y_pont  # lever arm, right propeller (m)
         self.k_pos = 0.02216 / 2  # Positive Bollard, one propeller
-        self.k_neg = 0.01289 / 2  # Negative Bollard, one propeller
+        # self.k_neg = 0.01289 / 2  # Negative Bollard, one propeller
+        self.k_port = 1
+        self.k_starboard = 1
 
-        # max. prop. rev.
-        self.n_max = np.sqrt((0.5 * 24.4 * self.g) / self.k_pos)
-        # min. prop. rev.
-        self.n_min = -np.sqrt((0.5 * 13.6 * self.g) / self.k_neg)
+        # # max. prop. rev.
+        # self.n_max = np.sqrt((0.5 * 24.4 * self.g) / self.k_pos)
+        # # min. prop. rev.
+        # self.n_min = -np.sqrt((0.5 * 13.6 * self.g) / self.k_neg)
 
         # MRB_CG = [ (m+mp) * I2  O2      (Fossen 2021, Chapter 3)
         #               O2        Ig ]
@@ -165,11 +173,11 @@ class OtterModel(Model):
         self.D = -np.diag(model_params["D_vector"])
 
         # Update thruster coefficients
-        self.k_pos = model_params["k_pos"]
-        self.k_neg = model_params["k_neg"]
+        self.k_port = model_params["k_port"]
+        self.k_starboard = model_params["k_starboard"]
+        # TODO: Add update to n_min and n_max as well
 
-    def step(self, x: ca.Opti.variable, prev_u: ca.Opti.variable,
-             u: ca.Opti.variable) -> tuple[ca.Opti.variable, ca.Opti.variable]:
+    def step(self, x: ca.Opti.variable, u: ca.Opti.variable) -> tuple[ca.Opti.variable, ca.Opti.variable]:
         """
         Step method
         [nu,u_feedback] = step(eta,nu,u_feedback,action,beta_c,V_c) integrates
@@ -179,9 +187,7 @@ class OtterModel(Model):
         -----------
             x : ca.Opti.variable
                 State space containing pose and velocity in 3-DOF
-            prev_u : np.ndarray
-                Previous control input
-            action : np.ndarray
+            u : np.ndarray
                 Current control input
 
         Returns
@@ -198,49 +204,15 @@ class OtterModel(Model):
         nu = x[3:]
 
         # Input vector
-        n = ca.vertcat(prev_u[0],
-                       prev_u[1])
+        n = ca.vertcat(u[0],
+                       u[1])
 
-        # Rigid body and added mass Coriolis and centripetal matrices
-        # CRB_CG = [ (m+mp) * Smtrx(nu2)          O3           (Fossen 2021, Chapter 6)
-        #              O3                   -Smtrx(Ig*nu2)  ]
-        # CRB_CG = np.zeros((3, 3))
-        # CRB_CG[0:2, 0:2] = self.m_total * s
-        # CRB_CG[-1, -1] = -Smtrx(np.matmul(self.Ig, nu[-1]))[-1, -1]
+        # Coriolis matrix
+        C = utils.opt.m2c(self.M, nu)
 
-        # Make CRB directly for simplicty
-        # -------------------------------------
-        # CRB = self.H_rg.T @ CRB_CG @ self.H_rg  # transform CRB from CG to CO
-        # # -------------------------------------
-
-        # CA = utils.m2c(self.MA, nu)
-        # print(f"CA with Munk moment: {CA}")
-        # CA[-1, 0] = 0  # assume that the Munk moment in yaw can be neglected
-        # CA[-1, 1] = 0  # if nonzero, must be balanced by adding nonlinear damping
-        # CA[0, -1] = 0
-        # CA[1, -1] = 0
-        # print(f"CA without Munk moment: {CA}")
-
-        C = utils.opt.m2c(self.M, nu)  # CRB + CA
-
-        # Control forces and moments - with propeller revolution saturation
-        # thrust = np.zeros(2)
-        # for i in range(0, 2):
-
-        # saturation, physical limits
-        # TODO: Check if saturation constrain in _init_opt is
-        #       sufficient or if we need something similar to
-        #       the line below
-        # ----------------------------------------------
-        # n[i] = utils.sat(self.n_min, n[i], self.n_max)
-        # ----------------------------------------------
-
-        # if n[i] > 0:  # positive thrust
-        #     thrust[i] = self.k_pos * n[i] * abs(n[i])
-        # else:  # negative thrust
-        #     thrust[i] = self.k_neg * n[i] * abs(n[i])
-        thrust = ca.vertcat(self.k_pos * n[0] * ca.fabs(n[0]),
-                            self.k_pos * n[1] * ca.fabs(n[1]))
+        # Linear thrust dynamics
+        thrust = ca.vertcat(self.k_port * n[0],
+                            self.k_starboard * n[1])
 
         # Control forces and moments
         tau = ca.vertcat(thrust[0] + thrust[1],
@@ -248,81 +220,19 @@ class OtterModel(Model):
                          -self.l1 * thrust[0] - self.l2 * thrust[1])
         # Hydrodynamic linear damping + nonlinear yaw damping
         tau_damp = -self.D @ nu
-        tau_damp[-1] = tau_damp[-1] - 10 * \
-            self.D[-1, -1] * ca.fabs(nu[-1]) * nu[-1]
 
-        # State derivatives (with dimension)
-        tau_crossflow = utils.opt.crossFlowDrag(
-            self.L, self.B_pont, self.T, nu)
-
+        # Solve the Fossen equation
         sum_tau = (
             tau
             + tau_damp
-            + tau_crossflow
             - C @ nu
         )
-
         nu_dot = self.Minv @ sum_tau
+
+        # Transform nu from {b} to {n}
         eta_dot = utils.opt.Rz(eta[-1]) @ nu
-        u_dot = (u - n) / self.T_n  # propeller dynamics
 
         x_dot = ca.vertcat(eta_dot,
                            nu_dot)
 
-        return x_dot, u_dot
-
-    def _denormalise(self, u):
-        if u[0] < 0:
-            u[0] = u[0]*111
-        else:
-            u[0] = u[0]*113
-
-        if u[1] < 0:
-            u[1] = u[1]*111
-        else:
-            u[1] = u[1]*113
-
-        return u
-
-    # def single_shooting(self, x_init, u_init, opti: ca.Opti):
-    #     # Declaring optimization variables
-    #     # State variables
-    #     x = opti.variable(3, self.N+1)
-    #     x_pos = x[0, :]
-    #     y_pos = x[1, :]
-    #     theta = x[2, :]
-
-    #     # Input variables
-    #     u = opti.variable(2, self.N)
-    #     v = u[0, :]
-    #     phi = u[1, :]
-
-    #     # Slack variables
-    #     s = opti.variable(3, self.N+1)
-
-    #     # Fixed step Runge-Kutta 4 integrator
-    #     self.RK4(x, u, self.N, opti)
-
-    #     # TODO: Put this code in a separate method
-    #     # ----------------------------------------------------
-    #     # Control signal and time constraint
-    #     opti.subject_to(opti.bounded(-1, v, 2))
-    #     opti.subject_to(opti.bounded(D2R(-15), phi, D2R(15)))
-    #     # ----------------------------------------------------
-
-    #     # Boundary values
-    #     # Initial conditions
-    #     opti.subject_to(x_pos[0] == x_init[0])
-    #     opti.subject_to(y_pos[0] == x_init[1])
-    #     opti.subject_to(theta[0] == x_init[2])
-    #     opti.subject_to(v[0] == u_init[0])
-    #     opti.subject_to(phi[0] == u_init[1])
-
-    #     # Initial guesses
-    #     opti.set_initial(x_pos, x_init[0])
-    #     opti.set_initial(y_pos, x_init[1])
-    #     opti.set_initial(theta, x_init[2])
-    #     opti.set_initial(v, u_init[0])
-    #     opti.set_initial(phi, u_init[1])
-
-    #     return x, u, s
+        return x_dot
